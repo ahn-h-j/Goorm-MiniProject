5293a4ffdf2fe125eaebfd19b293e059e5f01a04
ahn_h_j <tkgkd246@gmail.com>
2025-01-23 10:34:53 +0900

    feat(controller) : AdminController.java 추가
    관리자 전용 페이지의 Controller 구조 셋팅

---
+++ src/main/java/io/goorm/mini/admin/controller/AdminController.java
@@ -0,0 +1,35 @@
+package io.goorm.mini.admin.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/admins")
+    public String list(Model model){
+        return "admin/admin/list";
+    }
+
+    @GetMapping("/admins/{adminSeq}")
+    public String view(@PathVariable Long adminSeq, Model model){
+        return "admin/admin/view";
+    }
+
+    @PostMapping("/admins/create")
+    public String create(Model model){
+        return "admin/admin/create";
+    }
+
+    @GetMapping("/admins/modify")
+    public String modify(Model model){
+        return "admin/admin/modify";
+    }
+}
