f34792a8a5d8d96ef66a483cdee99346ebd49f00
ahn_h_j <tkgkd246@gmail.com>
2025-02-13 11:26:33 +0900

    feat : 회원가입 필터
    아이디 공백 확인
    중복체크 여부 확인
    아이디 중복 확인

--- src/main/resources/static/js/join.js
+++ src/main/resources/static/js/join.js
@@ -1,3 +1,29 @@
+document.querySelector("form").addEventListener("submit", function(event) {
+    var memberId = document.getElementById("memberId").value;
+    var isDuplicate = document.getElementById("isDuplicate").value;
+
+    // 아이디가 공백인지 확인
+    if (memberId.trim() === "") {
+        alert('아이디를 입력해주세요.');
+        event.preventDefault(); // 폼 제출을 중단
+        return;
+    }
+
+    // 중복 체크를 하지 않은 경우
+    if (isDuplicate === "") {
+        alert('아이디 중복 체크를 해주세요.');
+        event.preventDefault(); // 폼 제출을 중단
+        return;
+    }
+
+    // 아이디가 중복인 경우
+    if (isDuplicate === "true") {
+        alert('아이디가 중복됩니다.');
+        event.preventDefault(); // 폼 제출을 중단
+         return;
+    }
+});
+
 document.getElementById("checkBtn").addEventListener('click', function() {
 
     var memberId = document.getElementById("memberId").value;
